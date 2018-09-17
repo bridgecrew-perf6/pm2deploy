ba5e67a192a486c1a7c828425151e8b8ffe42c37
Aditya Pratama Nur <aditya.pratama.nur@gmail.com>
2018-09-17 11:40:20 +0700

    adding env

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import './index.css';
-
-export { default as Button } from './Button/Button';
-export { default as ButtonOval } from './Button/ButtonOval';
-export { default as ButtonRound } from './Button/ButtonRound';
