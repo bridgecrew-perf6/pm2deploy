d05a602c936f9974e647d864e081d2251c554cbd
Aditya Pratama Nur <aditya.pratama.nur@gmail.com>
2018-11-06 13:41:15 +0700

    adding example to lazyloading

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,9 +6,9 @@ const req = require.context("../src", true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
-addDecorator(
-  withInfo({
-    inline: true
-  })
-);
+// addDecorator(
+//   withInfo({
+//     inline: true
+//   })
+// );
 configure(loadStories, module);
--- src/templates/LazyImage/index.js
+++ src/templates/LazyImage/index.js
@@ -3,12 +3,9 @@ import PropTypes from "prop-types";
 import LazyLoad from "react-lazyload";
 import style from "./LazyImage.module.scss";
 
-const LazyImage = ({ alt, title, src }) => (
+const LazyImage = ({ alt, title, src, placeholder }) => (
   <div className={style.lazy}>
-    <LazyLoad
-      offset={150}
-      placeholder={<div className={style.lazy__loading}>Loading...</div>}
-    >
+    <LazyLoad offset={150} placeholder={placeholder}>
       <img alt={alt} title={title} src={src} />
     </LazyLoad>
   </div>
@@ -17,7 +14,12 @@ const LazyImage = ({ alt, title, src }) => (
 LazyImage.propTypes = {
   alt: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  src: PropTypes.string.isRequired
+  src: PropTypes.string.isRequired,
+  placeholder: PropTypes.element
+};
+
+LazyImage.defaultProps = {
+  placeholder: <div className={style.lazy__loading}> Loading...</div>
 };
 
 export default LazyImage;
