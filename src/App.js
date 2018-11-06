7bcbbb3c59815fede77a3a09d70fd8a0b9a3ceb9
Aditya Pratama Nur <aditya.pratama.nur@gmail.com>
2018-11-06 11:06:41 +0700

    experimenting with react suspense

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./scss/global.scss";
 import { Switch, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Error404 from "./pages/Error404";
-import ProductList from "./pages/ProductList";
+// import ProductList from "./pages/ProductList";
 import Header from "./components/Header/Header";
+
+const ProductList = lazy(() => import("./pages/ProductList"));
 
 const App = () => (
   <>
     <Header />
-    <Switch>
-      <Route exact path="/" component={Dashboard} />
-      <Route path="/product" component={ProductList} />
-      <Route path="/login" component={Login} />
-      <Route path="/list" component={Login} />
-      <Route component={Error404} />
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/product" component={ProductList} />
+        <Route path="/login" component={Login} />
+        <Route path="/list" component={Login} />
+        <Route component={Error404} />
+      </Switch>
+    </Suspense>
   </>
 );
 
