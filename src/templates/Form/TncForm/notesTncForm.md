6c957de88b0609e58aa67dafc4bbcebccca08900
Jojo <aditya.abdul@weekendinc.com>
2020-03-03 14:46:17 +0700

    add storybook tncform

---
+++ src/templates/Form/TncForm/notesTncForm.md
@@ -0,0 +1,17 @@
+<p>
+<b>Props:</b>
+<ul>
+<li>onSubmit, returning form values</li>
+<li>onSuccess</li>
+<li>onError</li>
+<li>errorMessage, object type</li>
+</ul>
+</p>
+<p>
+<hr />
+<b>How to use</b>
+<ul>
+<li>import <u>TncForm</u></li>
+<li>export as <u>TncForm(ViewComponent)</u></li>
+</ul>
+</p>
---
+++ src/templates/Form/TncForm/stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import TncFormView from "./TncFormView";
+import notes from "./notesTncForm.md";
+
+const act = values => console.log("Submitted", values);
+
+storiesOf("Form TNC", module).add(
+  "default",
+  () => <TncFormView onSubmit={action("eventSubmit", act)} />,
+  { notes }
+);
