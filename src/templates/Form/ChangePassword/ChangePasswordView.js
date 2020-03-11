b8dc41380a83b807823b8ef0fb8018d6d5dda99c
Jojo <aditya.abdul@weekendinc.com>
2020-03-11 15:47:53 +0700

    add changePassword stories

--- src/templates/Form/ChangePassword/ChangePasswordView.js
+++ src/templates/Form/ChangePassword/ChangePasswordView.js
@@ -1,8 +1,27 @@
 /* eslint linebreak-style: ["error", "windows"] */
 
 import React from "react";
+import PropTypes from "prop-types";
 import ChangePasswordForm from "./ChangePasswordForm";
 import Loading from "../../../components/Loading/Loading";
+import messageChangePassword from "./messageChangePassword";
+
+const { loginNameEmpty, loginNameWrongFormat, passwordEmpty, passwordMinChar, passwordNoNumber, passwordNoSpecial, passwordNoUppercase, passwordNotSame } = messageChangePassword;
+
+const defaultOnSubmit = values => {
+  console.log("eventSubmitted", values);
+  return { data: { status: 200, message: "success" }, error: {} };
+};
+
+const defaultOnSuccess = res => {
+  console.log("Success", res);
+  return res;
+};
+
+const defaultOnError = err => {
+  console.log("Error", err);
+  return err;
+};
 
 class ChangePasswordView extends React.Component {
   state = {};
@@ -59,5 +78,28 @@ class ChangePasswordView extends React.Component {
     );
   }
 }
+
+ChangePasswordView.defaultProps = {
+  onSubmit: defaultOnSubmit,
+  onSuccess: defaultOnSuccess,
+  onError: defaultOnError,
+  errorMessage: {
+    loginNameEmpty,
+    loginNameWrongFormat,
+    passwordEmpty,
+    passwordMinChar,
+    passwordNoNumber,
+    passwordNoSpecial,
+    passwordNoUppercase,
+    passwordNotSame
+  }
+};
+
+ChangePasswordView.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onSuccess: PropTypes.func,
+  onError: PropTypes.func,
+  errorMessage: PropTypes.object
+};
 
 export default ChangePasswordForm(ChangePasswordView);
